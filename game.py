74364cca8ae78df5a7b711e936ed11acfd2b75c4
myrmica-habilis <michalportes1@gmail.com>
2019-12-04 15:31:19 +0100

    Move classes to items.py - Chapter 10

--- game.py
+++ game.py
@@ -1,32 +1,4 @@
-class Weapon:
-    def __init__(self):
-        raise NotImplementedError("Do not create raw Weapon objects.")
-
-    def __str__(self):
-        return self.name
-
-
-class Rock(Weapon):
-    def __init__(self):
-        self.name = "Rock"
-        self.description = "A fist-sized rock, suitable for bludgeoning."
-        self.damage = 5
-
-
-class Dagger(Weapon):
-    def __init__(self):
-        self.name = "Dagger"
-        self.description = "A small dagger with some rust. " \
-                           "Somewhat more dangerous than a rock."
-        self.damage = 10
-
-
-class RustySword(Weapon):
-    def __init__(self):
-        self.name = "Rusty sword"
-        self.description = "This sword is showing its age, " \
-                           "but still has some fight in it."
-        self.damage = 20
+import items
 
 
 def most_powerful_weapon(inventory):
@@ -44,7 +16,7 @@ def most_powerful_weapon(inventory):
 
 
 def play():
-    inventory = [Rock(), Dagger(), 'Gold(5)', 'Crusty Bread']
+    inventory = [items.Rock(), items.Dagger(), 'Gold(5)', 'Crusty Bread']
     print("Escape from Cave Terror!")
     while True:
         action_input = get_player_command()
