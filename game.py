0e2afb801e210223d8a41d8af19eea977ddabae1
myrmica-habilis <michalportes1@gmail.com>
2019-12-04 11:54:53 +0100

    Add conditions - Chapter 4

--- game.py
+++ game.py
@@ -1,4 +1,14 @@
 print("Escape from Cave Terror!")
 
 action_input = input('Action: ')
-print(action_input)
+
+if action_input == 'n':
+    print("Go North!")
+elif action_input == 's':
+    print("Go South!")
+elif action_input == 'e':
+    print("Go East!")
+elif action_input == 'w':
+    print("Go West!")
+else:
+    print("Invalid action!")
