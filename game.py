c5af8f8cd8e60a4f973eea6a0c1246f4d4aab8fd
myrmica-habilis <michalportes1@gmail.com>
2019-12-04 15:42:44 +0100

    Add Player class, move logic to player.py - Chapter 10

--- game.py
+++ game.py
@@ -1,23 +1,9 @@
-import items
-
-
-def most_powerful_weapon(inventory):
-    max_damage = 0
-    best_weapon = None
-    for item in inventory:
-        try:
-            if item.damage > max_damage:
-                best_weapon = item
-                max_damage = item.damage
-        except AttributeError:
-            pass
-
-    return best_weapon
+from player import Player
 
 
 def play():
-    inventory = [items.Rock(), items.Dagger(), 'Gold(5)', 'Crusty Bread']
     print("Escape from Cave Terror!")
+    player = Player()
     while True:
         action_input = get_player_command()
         if action_input in ['n', 'N']:
@@ -29,9 +15,7 @@ def play():
         elif action_input in ['w', 'W']:
             print("Go West!")
         elif action_input in ['i', 'I']:
-            print("Inventory:")
-            for item in inventory:
-                print('*' + str(item))
+            player.print_inventory()
         else:
             print("Invalid action!")
 
