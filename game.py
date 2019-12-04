f8d33a86dd6c6c0c99a10a9a2c25692548fcab88
myrmica-habilis <michalportes1@gmail.com>
2019-12-04 12:12:35 +0100

    Add inventory - Chapter 6

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 def play():
+    inventory = ['Dagger', 'Gold(5)', 'Crusty Bread']
     print("Escape from Cave Terror!")
     action_input = get_player_command()
     if action_input == 'n' or action_input == 'N':
@@ -9,6 +10,9 @@ def play():
         print("Go East!")
     elif action_input == 'w' or action_input == 'W':
         print("Go West!")
+    elif action_input == 'i' or action_input == 'I':
+        print("Inventory:")
+        print(inventory)
     else:
         print("Invalid action!")
 
