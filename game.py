062e84924e7db5a40d462c5895b9b43407a240a7
myrmica-habilis <michalportes1@gmail.com>
2019-12-05 14:37:57 +0100

    Update main game loop - Chapter 11

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 from player import Player
+import world
 
 
 def play():
     print("Escape from Cave Terror!")
     player = Player()
     while True:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
         action_input = get_player_command()
         if action_input in ['n', 'N']:
-            print("Go North!")
+            player.move_north()
         elif action_input in ['s', 'S']:
-            print("Go South!")
+            player.move_south()
         elif action_input in ['e', 'E']:
-            print("Go East!")
+            player.move_east()
         elif action_input in ['w', 'W']:
-            print("Go West!")
+            player.move_west()
         elif action_input in ['i', 'I']:
             player.print_inventory()
         else:
