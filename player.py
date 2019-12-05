877164713b77433a05cd231b9ff6fcdea24dd5cd
myrmica-habilis <michalportes1@gmail.com>
2019-12-05 14:34:16 +0100

    Update Player class - Chapter 11

--- player.py
+++ player.py
@@ -5,6 +5,8 @@ class Player:
     def __init__(self):
         self.inventory = [items.Rock(), items.Dagger(),
                           'Gold(5)', 'Crusty Bread']
+        self.x = 1
+        self.y = 2
 
     def print_inventory(self):
         print("Inventory:")
@@ -25,3 +27,19 @@ class Player:
                 pass
 
         return best_weapon
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def move_north(self):
+        self.move(dx=0, dy=-1)
+
+    def move_south(self):
+        self.move(dx=0, dy=1)
+
+    def move_east(self):
+        self.move(dx=1, dy=0)
+
+    def move_west(self):
+        self.move(dx=-1, dy=0)
