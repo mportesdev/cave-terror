ea590ab8a89ce9249974a29b3452f69b975faa52
myrmica-habilis <michalportes1@gmail.com>
2019-12-05 14:58:01 +0100

    Add EnemyTile class, remove BoringTile - Chapter 12

--- world.py
+++ world.py
@@ -1,3 +1,8 @@
+import random
+
+import enemies
+
+
 class MapTile:
     def __init__(self, x, y):
         self.x = x
@@ -15,13 +20,6 @@ class StartTile(MapTile):
         """
 
 
-class BoringTile(MapTile):
-    def intro_text(self):
-        return """
-        This is a very boring part of the cave.
-        """
-
-
 class VictoryTile(MapTile):
     def intro_text(self):
         return """
@@ -33,11 +31,32 @@ class VictoryTile(MapTile):
         """
 
 
+class EnemyTile(MapTile):
+    def __init__(self, x, y):
+        r = random.random()
+        if r < 0.50:
+            self.enemy = enemies.GiantSpider()
+        elif r < 0.80:
+            self.enemy = enemies.Ogre()
+        elif r < 0.95:
+            self.enemy = enemies.BatColony()
+        else:
+            self.enemy = enemies.RockMonster()
+
+        super().__init__(x, y)
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return "A {} awaits!".format(self.enemy.name)
+        else:
+            return "You've defeated the {}.".format(self.enemy.name)
+
+
 world_map = [
     [None, VictoryTile(1, 0), None],
-    [None, BoringTile(1, 1), None],
-    [BoringTile(0, 2), StartTile(1, 2), BoringTile(2, 2)],
-    [None, BoringTile(1, 3), None]
+    [None, EnemyTile(1, 1), None],
+    [EnemyTile(0, 2), StartTile(1, 2), EnemyTile(2, 2)],
+    [None, EnemyTile(1, 3), None]
 ]
 
 
