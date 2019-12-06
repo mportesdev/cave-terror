f7658fce412b9494e96b1dfbc063323c7451f70b
myrmica-habilis <michalportes1@gmail.com>
2019-12-06 13:19:27 +0100

    Refactor selection of player's actions - Chapter 13

--- game.py
+++ game.py
@@ -1,5 +1,47 @@
+from collections import OrderedDict
+
 from player import Player
 import world
+
+
+def get_available_actions(room, player):
+    actions = OrderedDict()
+    print("Choose an action: ")
+    if player.inventory:
+        action_adder(actions, 'i', player.print_inventory, "Print inventory")
+    if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
+        action_adder(actions, 'a', player.attack, "Attack")
+    else:
+        if world.tile_at(room.x, room.y - 1):
+            action_adder(actions, 'n', player.move_north, "Go North")
+        if world.tile_at(room.x, room.y + 1):
+            action_adder(actions, 's', player.move_south, "Go South")
+        if world.tile_at(room.x + 1, room.y):
+            action_adder(actions, 'e', player.move_east, "Go East")
+        if world.tile_at(room.x - 1, room.y):
+            action_adder(actions, 'w', player.move_west, "Go West")
+    if player.hp < 100:
+        action_adder(actions, 'h', player.heal, "Heal")
+
+    return actions
+
+
+def action_adder(action_dict, hotkey, action, name):
+    action_dict[hotkey.lower()] = action
+    action_dict[hotkey.upper()] = action
+    print("{}: {}".format(hotkey, name))
+
+
+def choose_action(room, player):
+    action = None
+    while not action:
+        available_actions = get_available_actions(room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
+        else:
+            print("Invalid action!")
 
 
 def play():
@@ -9,27 +51,7 @@ def play():
         room = world.tile_at(player.x, player.y)
         print(room.intro_text())
         room.modify_player(player)
-        action_input = get_player_command()
-        if action_input in ['n', 'N']:
-            player.move_north()
-        elif action_input in ['s', 'S']:
-            player.move_south()
-        elif action_input in ['e', 'E']:
-            player.move_east()
-        elif action_input in ['w', 'W']:
-            player.move_west()
-        elif action_input in ['i', 'I']:
-            player.print_inventory()
-        elif action_input in ['a', 'A']:
-            player.attack()
-        elif action_input in ['h', 'H']:
-            player.heal()
-        else:
-            print("Invalid action!")
-
-
-def get_player_command():
-    return input('Action: ')
+        choose_action(room, player)
 
 
 play()
